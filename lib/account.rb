e74fba54ac1a3e4e9875dbf502e773f13c087f16
LindaKadz <achielinda44@gmail.com>
2018-05-30 11:49:28 +0300

    Testing failed transactions

--- lib/account.rb
+++ lib/account.rb
@@ -5,4 +5,19 @@ def initialize
   @balance= 1000
 end
 
+def withdraw(amount)
+	if (@balance < amount)
+   	'You do not have sufficient funds'
+
+  else
+    @balance -= amount
+    amount
+  end
+end
+
+def deposit (amount)
+	@balance += amount
+	amount
+end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,5 +3,28 @@ describe Account do
   it 'returns my balance' do
   expect(subject.balance).to eq 1000
   end
-
+  it 'returns my withdrawn amount' do
+  expect(subject.withdraw(900)).to eq 900
+  end
+  it 'returns 200 when I withdraw 200' do
+  	expect(subject.withdraw(200)).to eq 200
+  end
+  it 'reduces my balance' do
+  	subject.withdraw(400)
+  	expect(subject.balance).to eq 600
+  end
+  it 'returns amount deposited' do
+  	expect(subject.deposit(500)).to eq 500
+  end
+  it 'returns my new balance' do
+  	subject.deposit(500)
+  	expect(subject.balance).to eq 1500
+  end
+  it 'returns insufficient funds' do
+    expect(subject.withdraw(2000)).to eq 'You do not have sufficient funds'
+  end
+  it 'returns current balance' do
+    subject.withdraw(2000)
+    expect(subject.balance).to eq 1000
+  end
 end
