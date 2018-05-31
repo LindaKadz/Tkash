b0f76af07a68aa8ce813d040403aed699f94f900
alphonceakim <aliciakheem@gmail.com>
2018-05-31 13:57:54 +0300

    fix merge conflict

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,7 @@ describe Account do
   it 'returns my balance' do
   expect(subject.balance).to eq 1000
   end
+
   it 'reduces my balance' do
   	subject.withdraw(400, 34)
   	expect(subject.balance).to eq 600
@@ -30,22 +31,5 @@ describe Account do
   end
   it 'returns fail message for wrong agent number' do
   expect(subject.withdraw(200, 89)).to eq 'Failed, wrong agent number'
-  end
-  it 'returns my withdrawn amount' do
-  expect(subject.withdraw(900)).to eq 900
-  end
-  it 'returns 200 when I withdraw 200' do
-  	expect(subject.withdraw(200)).to eq 200
-  end 
-  it 'reduces my balance' do
-  	subject.withdraw(400)
-  	expect(subject.balance).to eq 600
-  end
-  it 'returns amount deposited' do
-  	expect(subject.deposit(500)).to eq 500
-  end
-  it 'returns my new balance' do
-  	subject.deposit(500)
-  	expect(subject.balance).to eq 1500
   end
 end
